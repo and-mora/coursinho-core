d3583ad9ef160935e2459196cfdc2016e02d0702
Danilo <danilo.lcq@gmail.com>
2021-03-05 15:40:19 +0100

    refactor

---
+++ src/main/java/it/bit/academy/corsopiu/entities/Attendance.java
@@ -0,0 +1,12 @@
+package it.bit.academy.corsopiu.entities;
+
+import java.time.LocalDateTime;
+
+public class Attendance {
+
+    private Student student;
+    private Lesson lesson;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+
+}
