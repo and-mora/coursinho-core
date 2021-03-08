db1998af62fcfe382b069c8247780a530313cdc1
andmora <and.morabito@gmail.com>
2021-03-08 11:42:12 +0100

    added getters and setters

--- src/main/java/it/bit/academy/corsopiu/dtos/CourseDto.java
+++ src/main/java/it/bit/academy/corsopiu/dtos/CourseDto.java
@@ -21,4 +21,60 @@ public class CourseDto {
         this.certification = course.isCertification();
         this.category = course.getCategory();
     }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getProgram() {
+        return program;
+    }
+
+    public void setProgram(String program) {
+        this.program = program;
+    }
+
+    public boolean isCertification() {
+        return certification;
+    }
+
+    public void setCertification(boolean certification) {
+        this.certification = certification;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
 }
