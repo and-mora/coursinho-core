e3001985c9b3d98ea719ef023ddbabe324fa5ef0
andmora <and.morabito@gmail.com>
2021-03-10 09:20:12 +0100

    fix query

--- src/main/java/it/bit/academy/corsopiu/repositories/ModuleRepository.java
+++ src/main/java/it/bit/academy/corsopiu/repositories/ModuleRepository.java
@@ -3,11 +3,13 @@ package it.bit.academy.corsopiu.repositories;
 import it.bit.academy.corsopiu.entities.Module;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Collection;
 
 public interface ModuleRepository extends JpaRepository<Module, Long> {
 
-    @Query("select m from Module as m where m.edition_id = :editionId")
-    Collection<Module> getByCouseEditionId(Long editionId);
+    // TO FIX
+    @Query("select m from Module as m where m.edition.getId() = :editionId")
+    Collection<Module> getByCouseEditionId(@Param("editionId") Long editionId);
 }
