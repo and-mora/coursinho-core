5a61ad61a7df110de117d6e3068d5a78be16e498
Andrea Morabito <andrea.morabito@pagopa.it>
2022-05-25 23:36:59 +0200

    [COUR-20] Implementation of basic course findFiltered

--- src/main/java/it/amorabito/coursinho/repositories/custom/CourseRepositoryCustomImpl.java
+++ src/main/java/it/amorabito/coursinho/repositories/custom/CourseRepositoryCustomImpl.java
@@ -2,15 +2,15 @@ package it.amorabito.coursinho.repositories.custom;
 
 import it.amorabito.coursinho.model.dtos.CourseFilter;
 import it.amorabito.coursinho.model.entities.Course;
+import it.amorabito.coursinho.model.entities.Course_;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Expression;
-import javax.persistence.criteria.Root;
+import javax.persistence.criteria.*;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 public class CourseRepositoryCustomImpl implements CourseRepositoryCustom {
 
@@ -24,21 +24,25 @@ public class CourseRepositoryCustomImpl implements CourseRepositoryCustom {
         Root<Course> root = criteriaQuery.from(Course.class);
 
         criteriaQuery.select(root);
-        criteriaQuery.where(applyFilter(root, courseFilter));
+        criteriaQuery.where(applyFilter(root, criteriaBuilder, courseFilter));
 
         TypedQuery<Course> query = entityManager.createQuery(criteriaQuery);
         return query.getResultList();
     }
 
-    private Expression<Boolean> applyFilter(Root<Course> root, CourseFilter courseFilter) {
-        // todo filters
+    private Predicate[] applyFilter(Root<Course> root, CriteriaBuilder criteriaBuilder, CourseFilter courseFilter) {
         if (courseFilter == null) {
-            return null;
+            return new Predicate[0];
         }
+
+        List<Predicate> predicates = new ArrayList<>();
 
         if (courseFilter.getMaximumPrice() != null) {
-
+            predicates.add(criteriaBuilder.lessThanOrEqualTo(root.get(Course_.price), courseFilter.getMaximumPrice()));
         }
-        return null;
+        if (courseFilter.getMinimumPrice() != null) {
+            predicates.add(criteriaBuilder.greaterThanOrEqualTo(root.get(Course_.price), courseFilter.getMinimumPrice()));
+        }
+        return predicates.toArray(new Predicate[0]);
     }
 }
